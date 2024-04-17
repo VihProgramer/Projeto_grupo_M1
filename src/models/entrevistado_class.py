0eea5d3f92c4fbffba13b47cfdb9dea0ba0c5e23
Ysmael Marques <ysmaelmarkss@gmail.com>
2024-04-17 15:25:13 -0300

    adicionando extras

--- src/models/entrevistado_class.py
+++ src/models/entrevistado_class.py
@@ -1,11 +1,14 @@
 from datetime import datetime
 
 class Entrevistado:
-    def __init__(self, genero, idade, resposta_1, resposta_2, resposta_3, resposta_4):
+    def __init__(self, genero, idade, resposta_1, resposta_2, resposta_3, resposta_4, resposta_extra_1, resposta_extra_2):
         self.genero = genero
         self.idade = idade
         self.resposta_1 = resposta_1
         self.resposta_2 = resposta_2
         self.resposta_3 = resposta_3
         self.resposta_4 = resposta_4
+        self.resposta_extra_1 = resposta_extra_1
+        self.resposta_extra_2 = resposta_extra_2
+
         self.data_hora = datetime.now()
--- src/script.py
+++ src/script.py
@@ -1,15 +1,15 @@
 import csv
 from src.models.entrevistado_class import *
 
-def input_de_dados(genero, idade, resposta_1, resposta_2, resposta_3, resposta_4):
+def input_de_dados(genero, idade, resposta_1, resposta_2, resposta_3, resposta_4, resposta_extra_1,resposta_extra_2):
     data = []
     try:
-        if not all([genero, idade, resposta_1, resposta_2, resposta_3, resposta_4]):            
+        if not all([genero, idade, resposta_1, resposta_2, resposta_3, resposta_4, resposta_extra_1,resposta_extra_2]):            
             return False
-        entrevistado = Entrevistado(genero, idade, resposta_1, resposta_2, resposta_3, resposta_4)
+        entrevistado = Entrevistado(genero, idade, resposta_1, resposta_2, resposta_3, resposta_4,resposta_extra_1, resposta_extra_2)
         return entrevistado
-    except:
-        print("Erro ao adicionar dados:")
+    except Exception as e:
+        print(f"Erro ao escrever dados: {e}")
         return False
 
 def escrever_csv(data):
@@ -17,7 +17,7 @@ def escrever_csv(data):
     try:
         with open(nome_arquivo, mode='w', newline='') as csvfile:
             writer = csv.writer(csvfile)
-            writer.writerow(['Genero', 'Idade', 'Resposta 1', 'Resposta 2', 'Resposta 3', 'Resposta 4', 'Data'])
+            writer.writerow(['Genero', 'Idade', 'Resposta 1', 'Resposta 2', 'Resposta 3', 'Resposta 4', 'Resposta Extra1', 'Resposta Extra2', 'Data'])
             for entrevistado in data:
                 writer.writerow(entrevistado)
         print(f'Dados foram escritos com sucesso no arquivo {nome_arquivo}.')
