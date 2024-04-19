d0a73c4d2729f4b13e8bf5abbd928ef5e612ffd6
Antonio Juevan <antoniojuevan@gmail.com>
2024-04-18 23:40:03 -0300

    Foi adicionado um página de gráficos e um botão home

--- src/script.py
+++ src/script.py
@@ -24,3 +24,35 @@ def escrever_csv(data):
     except Exception as e:
         print(f"Erro ao escrever dados no arquivo CSV: {e}")
 
+total_masculino = 0
+total_feminino = 0
+respostas = {
+    'Resposta 1': {'SIM': 0, 'NÃO': 0, 'NÃO SEI': 0},
+    'Resposta 2': {'SIM': 0, 'NÃO': 0, 'NÃO SEI': 0},
+    'Resposta 3': {'SIM': 0, 'NÃO': 0, 'NÃO SEI': 0},
+    'Resposta 4': {'SIM': 0, 'NÃO': 0, 'NÃO SEI': 0},
+    'Resposta 5': {'SIM': 0, 'NÃO': 0, 'NÃO SEI': 0},
+    'Resposta 6': {'SIM': 0, 'NÃO': 0, 'NÃO SEI': 0}
+}
+
+
+def contar_respostas(data):
+    global total_masculino, total_feminino, respostas
+
+    for resposta in data:
+        genero = resposta[0]
+        if genero == 'MASCULINO':
+            total_masculino += 1
+        elif genero == 'FEMININO':
+            total_feminino += 1
+
+        for i in range(2, len(resposta) - 1):
+            if i > 7:
+                continue
+            resposta_pergunta = resposta[i]
+            if resposta_pergunta in respostas[f'Resposta {i-1}']:
+                respostas[f'Resposta {i-1}'][resposta_pergunta] += 1
+
+
+
+
